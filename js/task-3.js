957c6a5cb9da6b8d3ad323a0cb1c436d6376eb53
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2023-12-14 18:20:08 +0000

    Fix task #3

--- js/task-3.js
+++ js/task-3.js
@@ -9,15 +9,15 @@ class StringBuilder {
   }
 
   padEnd(str) {
-    return (this.#value = str + this.#value);
+    this.#value += str;
   }
 
   padStart(str) {
-    return (this.#value += str);
+    this.#value = str + this.#value;
   }
 
   padBoth(str) {
-    return (this.#value = str + this.#value + str);
+    this.#value = str + this.#value + str;
   }
 }
 
